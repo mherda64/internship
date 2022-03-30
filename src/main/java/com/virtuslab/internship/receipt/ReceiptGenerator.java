be95cfcebad561952c024f406b9e02670eba8cc5
mherda64 <mherda64@gmail.com>
2022-03-30 17:56:48 +0200

    Fixed source code to make tests pass

--- src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
+++ src/main/java/com/virtuslab/internship/receipt/ReceiptGenerator.java
@@ -1,14 +1,21 @@
 package com.virtuslab.internship.receipt;
 
 import com.virtuslab.internship.basket.Basket;
+import com.virtuslab.internship.product.Product;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReceiptGenerator {
 
     public Receipt generate(Basket basket) {
-        List<ReceiptEntry> receiptEntries = new ArrayList<>();
+        Map<Product, Integer> productCount = basket.getProducts().stream()
+                .collect(Collectors.toMap(p -> p, p -> 1, Integer::sum));
+        List<ReceiptEntry> receiptEntries = basket.getProducts().stream()
+                .distinct()
+                .map(product -> new ReceiptEntry(product, productCount.get(product)))
+                .toList();
         return new Receipt(receiptEntries);
     }
 }
